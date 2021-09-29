f30cabd42a106a1597c9859c69cfc205e49b4ba9
andezitgq <sanerhp.mail@gmail.com>
2021-09-30 01:22:16 +0300

    Docs and links update

---
+++ docs/docs_en.md
@@ -0,0 +1,44 @@
+
+Default prefix - "yt"
+ 
+#### Music
+
+**1)** play:
+- _Decription_: Play music by title or link(Youtube);
+- _Usage_: ~~play~~ \<title\> or ~~play~~ \<link\>
+
+**3)** skip:
+- _Decription_: Skip the song currently playing
+- _Usage_: ~~skip~~
+
+**4)** join:
+- _Decription_: Connect to a voice channel;
+- _Usage_: ~~join~~ or ~~join~~ \<voice_channel\>
+
+**5)** pause:
+- _Decription_: Pause music playing;
+- _Usage_: ~~pause~~
+
+**6)** resume:
+- _Decription_: Resume playing music;
+- _Usage_: ~~resume~~
+
+**7)** remove:
+- _Decription_: Remove a song from the track queue;
+- _Usage_: ~~remove~~ \<number in the queue\>
+
+**8)** leave:
+- _Decription_: Stop music and leave the voice channel;
+- _Usage_: ~~leave~~
+
+**9)** list:
+- _Decription_: List of tracks in the queue;
+- _Usage_: ~~list~~
+
+**10)** currentsong:
+- _Decription_: Song currently playing;
+- _Usage_: ~~currentsong~~
+
+**10)** clear:
+- _Decription_: Clear entire track queue;
+- _Usage_: ~~clear~~
--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-/*$.ajax({
-	url: "https://top.gg/api/bots/882648343564652544/stats",
-	method: "GET",
-	dataType: "json",
-	crossDomain: true,
-	cache: false,
-	beforeSend: function (xhr) {
-		xhr.setRequestHeader("Authorization", config.TOKEN);
-	},
-	success: function (data) {
-		$('.server-count').html("Active servers: " + data.server_count);
-	},
-	error: function (jqXHR, textStatus, errorThrown) {
-		alert(errorThrown);
-	}
-});*/
+function file_get_contents( url ) {	// Reads entire file into a string
+	// 
+	// +   original by: Legaev Andrey
+	// %		note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
 
-$('.documentation').focus();
+	var req = null;
+	try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {
+		try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {
+			try { req = new XMLHttpRequest(); } catch(e) {}
+		}
+	}
+	if (req == null) throw new Error('XMLHttpRequest not supported');
+
+	req.open("GET", url, false);
+	req.send(null);
+
+	return req.responseText;
+}
+
+
+var docs_en = file_get_contents("./docs/docs_en.md");
+var docs_ru = file_get_contents("./docs/docs_ru.md");
+var docs_de = file_get_contents("./docs/docs_de.md");
+
+$('.doc-content').html(marked(docs_en));
+$('.doc-title').html('Documentation');
 
 $('#btn-ru').click(function(){
-	$('.doc-content').html('ru');
+	$('.doc-content').html(marked(docs_ru));
 	$('.doc-title').html('Документация');
 });
 
 $('#btn-en').click(function(){
-	$('.doc-content').html('en');
+	$('.doc-content').html(marked(docs_en));
 	$('.doc-title').html('Documentation');
 });
 
-$('#btn-esp').click(function(){
-	$('.doc-content').html('esp');
-	$('.doc-title').html('Dokumentado ');
+$('#btn-de').click(function(){
+	$('.doc-content').html(marked(docs_de));
+	$('.doc-title').html('Dokumentation');
+});
+
+$('.documentation').focus(function() {
+	alert( "Handler for .focus() called." );
 });
